2f0016bf50dbc288953c89aa529d9fb935824580
chenzhe <chenzhe@meicai.cn>
2019-07-29 10:31:13 +0800

    init project

---
+++ FooViewAlpha/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FooViewAlpha
+//
+//  Created by yunshan on 2019/7/29.
+//  Copyright © 2019 chenzhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FooViewAlpha/classes/FooViewAlphaManager.m
@@ -0,0 +1,13 @@
+//
+//  FooViewAlphaManager.m
+//  FooViewAlpha
+//
+//  Created by yunshan on 2019/7/29.
+//  Copyright © 2019 chenzhe. All rights reserved.
+//
+
+#import "FooViewAlphaManager.h"
+
+@implementation FooViewAlphaManager
+
+@end
